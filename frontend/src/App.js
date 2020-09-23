419c5ddefb90ac4456578b400bf09f26852e2da4
Daniela <londonoargottidaniela@gmail.com>
2020-09-23 23:43:57 +0000

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+   <h1> Parking zone </h1>
+   <h1> Hola </h1>
+    </div>
+  );
+}
+
+export default App;
